a784c3137782fefdcd9e0cf005d6dabfad3d7125
Frank Schüler <Frank.Schueler@fiduciagad.de>
2020-09-25 19:48:54 +0200

    bugfix at default html template of pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -16,15 +16,15 @@ const TEMPLATE = create(
 	`
 <nav class="pagination" jstl-if="\${pages.length > 1}">
 	<ul>
-		<li class="pagination-first \${page == 1 ? "disabled" : ""}" ?data-page="\${page != 1}" data-page="1">first</li>
-		<li class="pagination-back \${page == 1 ? "disabled" : ""}" ?data-page="\${page != 1}" data-page="\${page - 1}">prev</li>
+		<li class="pagination-first \${page == 1 ? 'disabled' : ''}" ?data-page="\${page != 1}" data-page="1">first</li>
+		<li class="pagination-back \${page == 1 ? 'disabled' : ''}" ?data-page="\${page != 1}" data-page="\${page - 1}">prev</li>
 
 		<jstl jstl-foreach="\${pages}">
-			<li class="pagination-page \${page == item ? "active" : ""}" ?data-page="\${page != item}" data-page="\${item}">\${item}</li>
+			<li class="pagination-page \${page == item ? "active" : ''}" ?data-page="\${page != item}" data-page="\${item}">\${item}</li>
 		</jstl>
 
-		<li class="pagination-next \${page >= count ? "disabled" : ""}" ?data-page="\${count > page}" data-page="\${page + 1}">next</li>
-		<li class="pagination-last \${page >= count ? "disabled" : ""}" ?data-page="\${count > page}" data-page="\${count}">prev</li>
+		<li class="pagination-next \${page >= count ? 'disabled' : ''}" ?data-page="\${count > page}" data-page="\${page + 1}">next</li>
+		<li class="pagination-last \${page >= count ? 'disabled' : ''}" ?data-page="\${count > page}" data-page="\${count}">prev</li>
 	</ul>
 </nav>
 `,
