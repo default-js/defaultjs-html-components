7288d3470eaed4bd460898947d27c97124bdd8f8
Frank Schüler <SchuelerFrank@gmx.de>
2021-09-16 11:55:10 +0200

    release 2.1.2
    removed unused dependencies
    removed TemplateHelper -> this capability is provided by defaultjs-template-language (Template.loadNodeTemplate(node, defaultTemplate, cache, alias))

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,6 @@ import DefineComponentHelper from "./DefineComponentHelper";
 import EventHelper from "./EventHelper";
 import NodeHelper from "./EventHelper";
 import StyleHelper from "./StyleHelper";
-import TemplateHelper from "./TemplateHelper";
 import WeakData from "./WeakData";
 
-export default {DefineComponentHelper, EventHelper, NodeHelper, StyleHelper, TemplateHelper, WeakData};
+export default {DefineComponentHelper, EventHelper, NodeHelper, StyleHelper, WeakData};
